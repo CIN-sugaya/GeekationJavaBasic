680bdd5504193056fd108cde8835285c1ae930aa
CIN-sugaya <mtg.sugaya@gmail.com>
2024-04-22 18:09:46 +0900

    インデント修正

--- src/basicSyntax/IfSwitch.java
+++ src/basicSyntax/IfSwitch.java
@@ -2,7 +2,7 @@ package basicSyntax;
 
 public class IfSwitch {
 
-	public static void main(String[] args) {
+	 public static void main(String[] args) {
 		int num =2;
 		
 		if(num % 2 == 0) {
@@ -12,25 +12,26 @@ public class IfSwitch {
 		}else {
 			System.out.println("どちらでもありません。");
 		}
-	String pref = "京都";
+		
+	    String pref = "京都";
 	
-	switch (pref){
-	 case "北海":
-		System.out.println("北海道");
-		break;
-	 case "東京":
-	    System.out.println("東京都");
-	    break;
-	 case "京都":
-	    System.out.println("京都府");
-	    break;
-	    default:
-	    System.out.println("当てはまりません。");
-	    break;
+	    switch (pref){
+	        case "北海":
+	        	System.out.println("北海道");
+		        break;
+	        case "東京":
+	            System.out.println("東京都");
+	            break;
+	        case "京都":
+	             System.out.println("京都府");
+	             break;
+	        default:
+	             System.out.println("当てはまりません。");
+	        break;
 	    
-	}
+	     }
 		
 	
-	}
+	 }
 
 }
